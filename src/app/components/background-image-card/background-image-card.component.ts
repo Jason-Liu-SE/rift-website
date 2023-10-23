6ec63c595180813488ed77c09900c71dd24322ab
Jason Liu <jasonliu2468@hotmail.com>
2023-10-23 03:28:07 -0400

    added overlay text over the background card

--- src/app/components/background-image-card/background-image-card.component.ts
+++ src/app/components/background-image-card/background-image-card.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-background-image-card',
-  templateUrl: './background-image-card.component.html',
-  styleUrls: ['./background-image-card.component.css']
+    selector: 'app-background-image-card',
+    templateUrl: './background-image-card.component.html',
+    styleUrls: ['./background-image-card.component.css']
 })
-export class BackgroundImageCardComponent {
-
-}
+export class BackgroundImageCardComponent {}
