47af913879b3c13e460ed4e51e8f6b9e590c63b7
Jason Liu <jasonliu2468@hotmail.com>
2023-10-25 00:17:31 -0400

    removed a console.log artifact in the navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
     @Input('transparent') public isTransparent: boolean = true;
-
-    ngOnInit() {
-        console.log(this.isTransparent);
-    }
 }
