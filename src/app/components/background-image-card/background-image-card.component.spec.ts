4866261f5b59531fbcf4b4a22c42a3122cf88f89
Jason Liu <jasonliu2468@hotmail.com>
2023-10-26 06:44:25 -0400

    visual changes to the welcome page

--- src/app/components/background-image-card/background-image-card.component.spec.ts
+++ src/app/components/background-image-card/background-image-card.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BackgroundImageCardComponent } from './background-image-card.component';
 
 describe('BackgroundImageCardComponent', () => {
-  let component: BackgroundImageCardComponent;
-  let fixture: ComponentFixture<BackgroundImageCardComponent>;
+    let component: BackgroundImageCardComponent;
+    let fixture: ComponentFixture<BackgroundImageCardComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [BackgroundImageCardComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [BackgroundImageCardComponent]
+        });
+        fixture = TestBed.createComponent(BackgroundImageCardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(BackgroundImageCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
