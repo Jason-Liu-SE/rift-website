c1cac04a6c24cfff21aaa7359b9f418cf78fee48
Jason Liu <jasonliu2468@hotmail.com>
2023-10-28 03:31:57 -0400

    creation and displaying a caption-card and collection-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { WelcomeCardComponent } from './components/welcome-card/welcome-card.component';
 import { TextSectionCardComponent } from './components/section-card/section-card.component';
 import { CaptionCardComponent } from './components/caption-card/caption-card.component';
+import { CollectionCardComponent } from './components/gallery/collection-card/collection-card.component';
 
 @NgModule({
     declarations: [
@@ -27,7 +28,8 @@ import { CaptionCardComponent } from './components/caption-card/caption-card.com
         PageTemplateDirective,
         WelcomeCardComponent,
         TextSectionCardComponent,
-        CaptionCardComponent
+        CaptionCardComponent,
+        CollectionCardComponent
     ],
     imports: [
         BrowserModule,
