91e55ed325baadcb37fc1b71f9e2ae9194f244b0
Jason Liu <jasonliu2468@hotmail.com>
2023-10-30 04:55:42 -0400

    basic implementation of caption-card and displaying them on the gallery page

--- src/app/components/caption-card/caption-card.component.ts
+++ src/app/components/caption-card/caption-card.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-caption-card',
-  templateUrl: './caption-card.component.html',
-  styleUrls: ['./caption-card.component.scss']
+    selector: 'app-caption-card',
+    templateUrl: './caption-card.component.html',
+    styleUrls: ['./caption-card.component.scss']
 })
-export class CaptionCardComponent {
+export class CaptionCardComponent implements OnInit {
+    @Input({ alias: 'src', required: true }) public imageUrl!: string;
+    @Input('alt') public altDesc: string = '';
+    @Input() public caption: string = '';
+    @Input('caption-visibility') visibility!: string; // visible, hidden, hover. Default = visible
 
+    public visibilityType!: string;
+
+    ngOnInit(): void {
+        switch (this.visibility) {
+            case 'hidden':
+                this.visibilityType = 'hidden';
+                break;
+            case 'hover':
+                this.visibilityType = 'hover';
+                break;
+            default:
+                this.visibilityType = 'visible';
+        }
+    }
 }
