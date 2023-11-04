302fdfc934989c38e54c3450e2baf795d4e541da
Jason Liu <jasonliu2468@hotmail.com>
2023-11-04 07:19:26 -0400

    added a fade effect for download-cards

--- src/app/components/download-card/download-card.component.ts
+++ src/app/components/download-card/download-card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-download-card',
-  templateUrl: './download-card.component.html',
-  styleUrls: ['./download-card.component.scss']
+    selector: 'app-download-card',
+    templateUrl: './download-card.component.html',
+    styleUrls: ['./download-card.component.scss']
 })
-export class DownloadCardComponent {
+export class DownloadCardComponent implements OnInit {
+    @Input() title: string = '';
+    @Input() desc: string = '';
+    @Input({ alias: 'src', required: true }) downloadUrl!: string;
 
+    public showTitle: boolean = false;
+    public showDesc: boolean = false;
+
+    ngOnInit(): void {
+        if (this.title !== '') {
+            this.showTitle = true;
+        }
+
+        if (this.desc !== '') {
+            this.showDesc = true;
+        }
+    }
 }
