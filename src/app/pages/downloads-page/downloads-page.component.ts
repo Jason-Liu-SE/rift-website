f76f95f6816499f9ab935ce9bb396f03e345b967
Jason Liu <jasonliu2468@hotmail.com>
2023-11-04 06:20:37 -0400

    created a basic hierarchy for the download-card component

--- src/app/pages/downloads-page/downloads-page.component.ts
+++ src/app/pages/downloads-page/downloads-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-downloads-page',
-  templateUrl: './downloads-page.component.html',
-  styleUrls: ['./downloads-page.component.scss']
+    selector: 'app-downloads-page',
+    templateUrl: './downloads-page.component.html',
+    styleUrls: ['./downloads-page.component.scss']
 })
 export class DownloadsPageComponent {
-
+    public collections: number[] = Array(3).fill(0);
 }
