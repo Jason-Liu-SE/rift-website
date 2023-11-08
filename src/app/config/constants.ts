64ef2e59d5e3ce8683503c8a2d97e810238ea20e
Jason Liu <jasonliu2468@hotmail.com>
2023-11-08 04:05:57 -0500

    updated the global constants to include the bluemap address

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -2,5 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Constants {
-    public readonly API_ENDPOINT: string = 'localhost:3000';
+    public readonly API_ENDPOINT: string = 'localhost:4000';
+    public readonly BLUEMAP_URL: string = 'http://localhost:8100/';
 }
