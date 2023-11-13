4d3ceb9c5f605d5fee451df2cf3f16cff576c89c
Jason Liu <jasonliu2468@hotmail.com>
2023-11-13 03:51:22 -0500

    moved data to mongoDB server

--- src/app/services/downloads-provider.service.ts
+++ src/app/services/downloads-provider.service.ts
@@ -7,56 +7,7 @@ import { IDownloadCollection } from '../interfaces/download-collection.interface
 export class DownloadsProviderService {
     private downloadCollections: IDownloadCollection[];
 
-    constructor() {
-        this.downloadCollections = [
-            {
-                name: 'Worlds',
-                items: [
-                    {
-                        title: 'Season 1',
-                        desc: 'Meh',
-                        downloadUrl: 'https://www.google.com',
-                        backgroundUrl: '../../../assets/home/1.png'
-                    },
-                    {
-                        title: 'Season 2',
-                        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
-                        downloadUrl: 'https://www.google.com',
-                        backgroundUrl: '../../../assets/home/2.png'
-                    },
-                    {
-                        title: 'Season 3',
-                        desc: 'Meh',
-                        downloadUrl: 'https://www.google.com',
-                        backgroundUrl: '../../../assets/home/3.png'
-                    }
-                ]
-            },
-            {
-                name: 'Other Stuff...',
-                items: [
-                    {
-                        title: 'Season 1',
-                        desc: 'BASewe wer',
-                        downloadUrl: 'https://www.google.com',
-                        backgroundUrl: '../../../assets/home/2.png'
-                    },
-                    {
-                        title: 'Season 2',
-                        desc: 'ASD ASD ASDASD',
-                        downloadUrl: 'https://www.google.com',
-                        backgroundUrl: '../../../assets/home/1.png'
-                    },
-                    {
-                        title: 'Season 3',
-                        desc: 'JHASDh jashjd kljASJLHD asjlkd ',
-                        downloadUrl: 'https://www.google.com',
-                        backgroundUrl: '../../../assets/home/3.png'
-                    }
-                ]
-            }
-        ];
-    }
+    constructor() {}
 
     public getDownloadCollections(): IDownloadCollection[] {
         return this.downloadCollections;
