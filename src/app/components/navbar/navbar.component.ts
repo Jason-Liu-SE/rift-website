661f88731da9343dfb964e7024fa6fca406480df
Jason Liu <jasonliu2468@hotmail.com>
2023-11-17 07:40:22 -0500

    made the navbar responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
 
 @Component({
@@ -9,9 +9,28 @@ import { ViewportScroller } from '@angular/common';
 export class NavbarComponent {
     @Input('transparent') public isTransparent: boolean = true;
 
+    public isHamburgerMenuVisible: boolean = false;
+
     constructor(private viewportScroller: ViewportScroller) {}
 
     public handlePageScroll(elemId: string): void {
         this.viewportScroller.scrollToAnchor(elemId);
+    }
+
+    public toggleHamburgerMenu(): void {
+        this.isHamburgerMenuVisible = !this.isHamburgerMenuVisible;
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+        const breakpoint: number =
+            60 *
+            parseFloat(getComputedStyle(document.documentElement).fontSize);
+        const elem: any = event.target;
+
+        // disabling the hamburger menu when the window size is greater than 60rem
+        if (elem.innerWidth >= breakpoint) {
+            this.isHamburgerMenuVisible = false;
+        }
     }
 }
